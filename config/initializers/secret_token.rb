3aa6e919ec47a85d5a479911be9ff22a476a080a
Giedrius Rimkus <info@giedriusr.lt>
2013-06-05 16:40:59 +0300

    initial commit for new RallyLive application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rallylive::Application.config.secret_key_base = '7037b2a8ac5306dd6adf382178dc65395e9c643b15d356ec8eb87ed32d3e543a6cc98e9c4b376403e2ddb51be1b1b4eba5e52cde0c11308b009dfdbf3683a60f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rallylive::Application.config.session_store :cookie_store, key: '_rallylive_session'
