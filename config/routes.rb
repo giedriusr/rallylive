d3029a92d5ebd114111fa83dcac89e13535861c2
Giedrius Rimkus <info@giedriusr.lt>
2013-06-06 00:13:13 +0300

    adding devise to the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rallylive::Application.routes.draw do
 
+  devise_for :users
+  devise_scope :user do
+    post '/sign_out', to: 'devise/sessions#destroy', as: 'sign_out'
+    get '/sign_in', to: 'devise/sessions#new', as: 'sign_in'
+    get '/users/sign_in', to: 'devise/sessions#new', as: 'users_sign_in'
+    get '/sign_up', to: 'devise/registrations#new', as: 'sign_up'
+    get '/reset_password', to: 'devise/passwords#new', as: 'reset_password'
+    get '/user/account', to: 'devise/registrations#edit', as: 'account'
+  end
   root 'home#index'
   get '/apie_mus', to: 'home#about_us', as: 'about_us'
 end
