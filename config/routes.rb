5d08e87286c78b2646730be05d8b6d3494542ff3
Giedrius Rimkus <info@giedriusr.lt>
2013-06-10 02:28:54 +0300

    blokuojam signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rallylive::Application.routes.draw do
     post '/sign_out', to: 'devise/sessions#destroy', as: 'sign_out'
     get '/sign_in', to: 'devise/sessions#new', as: 'sign_in'
     get '/users/sign_in', to: 'devise/sessions#new', as: 'users_sign_in'
-    get '/sign_up', to: 'devise/registrations#new', as: 'sign_up'
+    #get '/sign_up', to: 'devise/registrations#new', as: 'sign_up'
     get '/reset_password', to: 'devise/passwords#new', as: 'reset_password'
     get '/user/account', to: 'devise/registrations#edit', as: 'account'
   end
