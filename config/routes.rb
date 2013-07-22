916ea0db706647bdb782a49f3752a1fcf9625949
Giedrius Rimkus <info@giedriusr.lt>
2013-07-22 11:40:33 +0300

    url change

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rallylive::Application.routes.draw do
   get '/dalyviu_sarasas', to: 'home#participants', as: 'participants'
   get '/starto_tvarka', to: 'home#start_order', as: 'start_order'
   get '/greicio_ruozai', to: 'home#special_stages', as: 'special_stages'
-  get '/saules_ralis', to: 'home#show_rally', as: 'show_rally'
+  get '/apie_rali', to: 'home#show_rally', as: 'show_rally'
   get '/kalendorius', to: 'home#calendar', as: 'calendar'
   get '/lentele', to: 'home#standings', as: 'standings'
   get '/pasitraukimai', to: 'home#retirements', as: 'retirements'
